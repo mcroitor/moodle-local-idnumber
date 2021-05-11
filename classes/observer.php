51fea8a6519e5e308f468efbb93c5ff7b4451867
Mihail Croitor <mcroitor@gmail.com>
2021-05-11 16:19:43 +0300

    module generates idnumbers for questions and question categories as uuid.

---
+++ classes/observer.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace local_uuid;
+
+/**
+ * Event observer class.
+ *
+ * @package     local_idnumber
+ * @category    event
+ * @copyright   2021 Mihail Croitor <mcroitor@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class observer {
+
+    /**
+     * Triggered via $event.
+     *
+     * @param \core\event\question_category_created $event The event.
+     * @return bool True on success.
+     */
+    public static function question_category_created($event) {
+        global $DB;
+        if(isset($event->objectid)) {
+            $DB->execute(
+                "UPDATE {question_categories} SET idnumber=:idnumber WHERE id=:id",
+                ["id" => $event->objectid, "idnumber" => \core\uuid::generate()]
+            );
+        }
+        return true;
+    }
+
+    /**
+     * Triggered via $event.
+     *
+     * @param \core\event\question_created $event The event.
+     * @return bool True on success.
+     */
+    public static function question_created($event) {
+        global $DB;
+        if(isset($event->objectid)) {
+            $DB->execute(
+                "UPDATE {question} SET idnumber=:idnumber WHERE id=:id",
+                ["id" => $event->objectid, "idnumber" => \core\uuid::generate()]
+            );
+        }
+        return true;
+    }
+}
