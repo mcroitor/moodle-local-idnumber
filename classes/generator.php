b3ec31cdf360e6c5e7b87b315d36b365bbe6992c
Mihail Croitor <mcroitor@gmail.com>
2023-06-20 12:04:02 +0300

    fix naming

--- classes/generator.php
+++ classes/generator.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace local_uuid;
+namespace local_idnumber;
 
 class generator {
     /**
      * Generate a UUID for the question.
      *
-     * @param int $questionid The ID of the question.
+     * @param int $question_id The ID of the question.
      * @return string The generated UUID.
      */
-    public static function uuid($questionid) {
-        return uniqid();
+    public static function uuid($question_id) {
+        return \core\uuid::generate();
     }
 }
